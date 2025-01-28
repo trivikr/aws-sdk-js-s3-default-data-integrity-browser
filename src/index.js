6e8cd1a67a7c2dd0df7e86ab89813db73e956e43
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-01-28 10:30:40 -0800

    chore: pass custom ReadableStream in PutObject Body

--- src/index.js
+++ src/index.js
@@ -20,7 +20,25 @@ document.getElementById("form").addEventListener("submit", async (e) => {
   const response = await client.putObject({
     Bucket: import.meta.env.VITE_AWS_S3_BUCKET_NAME,
     Key: file.name,
-    Body: file.stream(),
+    Body: new ReadableStream({
+      pull(controller) {
+        let counter = 0;
+        const maxCount = 5;
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            if (counter < maxCount) {
+              const chunk = `Data chunk ${counter + 1}`;
+              controller.enqueue(chunk);
+              counter++;
+              resolve();
+            } else {
+              controller.close();
+              resolve();
+            }
+          }, 1000);
+        });
+      },
+    }),
   });
 
   console.log("Response", response);
